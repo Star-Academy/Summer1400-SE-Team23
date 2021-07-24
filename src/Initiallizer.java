5f7bd6ead8feccdd0f3ca9aa6142dbe5fc4cec10
navidnt <navid.nader.6n4@gmail.com>
2021-07-24 15:08:57 +0430

    'initiazer'

---
+++ src/Initiallizer.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Initiallizer {
+    private HashMap<String, ArrayList<String>> dictionary;
+
+    public void initialize(HashMap <String, String> documentsContent){
+        dictionary = new HashMap<>();
+        for(String s : documentsContent.keySet()){
+            String s1 = documentsContent.get(s);
+            String[] s2 = s1.split(" ");
+            for(String s3 : s2) {
+                ArrayList<String> documentsSet = dictionary.get(s3);
+                if(documentsSet.lastIndexOf(s) == -1){
+                    documentsSet.add(s);
+                }
+                dictionary.put(s3, documentsSet);
+            }
+        }
+    }
+
+}
